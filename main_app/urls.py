2a51c1674c5b30af2346ae74ebb6d6800cc7d00b
ehsan <1374ea@gmail.com>
2021-07-21 16:19:07 +0430

    redirect home url to naming:select-framework

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('naming/', include('apps.naming.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('naming:select-framework'), permanent=False), name='home'),
 ]
